81a693b0e3f898ea6ea210604a91248fdb362a6d
gaabrenno <gaabrenno@gmail.com>
2024-10-28 12:35:31 -0300

    develop: alt

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [
+
+
+        
 RegisteredUserController::class, 'create'])
                         ->name('register');
 
