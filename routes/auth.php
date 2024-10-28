259453e16c5d91066f041244126bb3cc3adb082d
gaabrenno <gaabrenno@gmail.com>
2024-10-28 12:35:00 -0300

    develop: alt

--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Auth\{AuthenticatedSessionController, ConfirmablePasswo
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('register', [
+RegisteredUserController::class, 'create'])
                         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
