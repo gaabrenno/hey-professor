6943b80d48e3de965458a4a274827378ecb0d9da
Gabriel <gaabrenno@gmail.com>
2024-11-06 21:56:30 -0300

    creating seeder and fatory for question model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Question;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -30,6 +31,8 @@ class UserFactory extends Factory
             'password'          => static::$password ??= Hash::make('password'),
             'remember_token'    => Str::random(10),
         ];
+
+        Question::factory()->count(10)->create();
     }
 
     /**
