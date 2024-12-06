68b68b49e5b64491e1e8c973018b4bbce0090f95
gaabrenno <gaabrenno@gmail.com>
2024-12-06 14:41:20 -0300

    creating seeder and fatory for question model real

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Question;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -32,7 +31,6 @@ class UserFactory extends Factory
             'remember_token'    => Str::random(10),
         ];
 
-        Question::factory()->count(10)->create();
     }
 
     /**
