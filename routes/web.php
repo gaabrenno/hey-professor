3eb51217655d793b5e543a3287d3aaab9b1f2fcf
gaabrenno <gaabrenno@gmail.com>
2024-12-06 17:36:30 -0300

    alt

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     if (app()->isLocal()) {
         auth()->loginUsingId(1);
-        (1);
 
         return to_route('dashboard');
     }
