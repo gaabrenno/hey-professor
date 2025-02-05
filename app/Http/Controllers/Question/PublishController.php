21a38b5e98647843c2931baa56dda136ebd34b64
Gabriel <gaabrenno@gmail.com>
2025-02-04 22:55:24 -0300

    adding RedirectResponse to return of function invoke to Publish Controller

--- app/Http/Controllers/Question/PublishController.php
+++ app/Http/Controllers/Question/PublishController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
+use Illuminate\Http\RedirectResponse;
 
 class PublishController extends Controller
 {
-    public function __invoke(Question $question)
+    public function __invoke(Question $question): RedirectResponse
     {
         $question->update(['draft' => false]);
 
