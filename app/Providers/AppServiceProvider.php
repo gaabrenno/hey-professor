c71ee4bc6d2e624f43d2801c69a0819a51e4f888
Gabriel <gaabrenno@gmail.com>
2025-02-27 19:09:26 -0300

    alt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         Model::preventLazyLoading(!app()->isProduction());
-        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
-            $event->extendSocialite('google', \SocialiteProviders\Google\Provider::class);
-        });
+        // Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+        //     $event->extendSocialite('google', \SocialiteProviders\Google\Provider::class);
+        // });
     }
 }
