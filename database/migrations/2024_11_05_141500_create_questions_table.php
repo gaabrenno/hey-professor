f2573e2283450c57302916b9865a0a0b53e41794
Gabriel <gaabrenno@gmail.com>
2025-03-08 22:25:42 -0300

    feat: add title field to questions model, migration, factory, and view

--- database/migrations/2024_11_05_141500_create_questions_table.php
+++ database/migrations/2024_11_05_141500_create_questions_table.php
@@ -14,6 +14,7 @@ return new class () extends Migration {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class, 'created_by');
+            $table->string('title', 255);
             $table->text('question');
             $table->boolean('draft')->default(false);
             $table->timestamps();
